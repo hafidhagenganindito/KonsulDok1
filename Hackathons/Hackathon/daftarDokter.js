672dcb289055ef980972d0b887fad4de95550cb1
Hafidh Ageng Anindito <hafidhdito007@gmail.com>
2023-12-22 07:54:41 +0700

    first commit

---
+++ Hackathons/Hackathon/daftarDokter.js
@@ -0,0 +1,232 @@
+const dokter = {
+  jantung: [
+    {
+      nama: "Dr.Nadia Kirana Sp.JP",
+      tanggal: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+      ],
+      img: "./assets/img/Dokter/1.jpg",
+      spesialis: "Dokter Jantung",
+      id: "1",
+    },
+    {
+      nama: "Dr.Sarah Sehan Sp.JP",
+      tanggal: [
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+        29, 30, 31,
+      ],
+      img: "./assets/img/Dokter/2.jpg",
+      spesialis: "Dokter Jantung",
+      id: "2",
+    },
+    {
+      nama: "Dr.Abullah Salim Sp.JP",
+      tanggal: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        31,
+      ],
+      img: "./assets/img/Dokter/15.jpg",
+      spesialis: "Dokter Jantung",
+      id: "3",
+    },
+  ],
+  anak: [
+    {
+      nama: "Dr.Berliana Murni Sp.A",
+      tanggal: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+      ],
+      img: "./assets/img/Dokter/18.jpg",
+      spesialis: "Dokter Anak",
+      id: "4",
+    },
+    {
+      nama: "Dr.Maria Johnson Sp.A",
+      tanggal: [
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+        29, 30, 31,
+      ],
+      img: "./assets/img/Dokter/3.jpg",
+      spesialis: "Dokter Anak",
+      id: "5",
+    },
+    {
+      nama: "Dr.Lewis Dunk Sp.A",
+      tanggal: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        31,
+      ],
+      img: "./assets/img/Dokter/4.jpg",
+      spesialis: "Dokter Anak",
+      id: "6",
+    },
+  ],
+  tht: [
+    {
+      nama: "Dr. Heung-Min-Son Sp.THT",
+      tanggal: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+      ],
+      img: "./assets/img/Dokter/14.jpg",
+      spesialis: "Dokter THT",
+      id: "7",
+    },
+    {
+      nama: "Dr. Budi Permono Sp.THT",
+      tanggal: [
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+        29, 30, 31,
+      ],
+      img: "./assets/img/Dokter/10.jpeg",
+      spesialis: "Dokter THT",
+      id: "8",
+    },
+    {
+      nama: "Dr.Yunita Sari Sp.THT",
+      tanggal: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        31,
+      ],
+      img: "./assets/img/Dokter/11.jpg",
+      spesialis: "Dokter THT",
+      id: "9",
+    },
+  ],
+  kulit: [
+    {
+      nama: "Dr Chris Edwin SpK",
+      tanggal: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+      ],
+      img: "./assets/img/Dokter/7.jpg",
+      spesialis: "Dokter Kulit",
+      id: "10",
+    },
+    {
+      nama: "Dr.Aldo Saputra Spk",
+      tanggal: [
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+        29, 30, 31,
+      ],
+      img: "./assets/img/Dokter/8.jpg",
+      spesialis: "Dokter Kulit",
+      id: "11",
+    },
+    {
+      nama: "Dr.Geraldine Putri SpK",
+      tanggal: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        31,
+      ],
+      img: "./assets/img/Dokter/12.jpg",
+      spesialis: "Dokter Kulit",
+      id: "12",
+    },
+  ],
+  mata: [
+    {
+      nama: "Dr. Indah Purnama Sp.M",
+      tanggal: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+      ],
+      img: "./assets/img/Dokter/5.jpg",
+      spesialis: "Dokter Mata",
+      id: "13",
+    },
+    {
+      nama: "Dr.William Hartono Sp.M",
+      tanggal: [
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+        29, 30, 31,
+      ],
+      img: "./assets/img/Dokter/6.jpg",
+      spesialis: "Dokter Mata",
+      id: "14",
+    },
+    {
+      nama: "Dr.Kim-Min-Jae Sp.M",
+      tanggal: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        31,
+      ],
+      img: "./assets/img/Dokter/17.jpg",
+      spesialis: "Dokter Mata",
+      id: "15",
+    },
+  ],
+  paru: [
+    {
+      nama: "Dr.Amar Saputra Sp.P",
+      tanggal: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+      ],
+      img: "./assets/img/Dokter/20.jpg",
+      spesialis: "Dokter Paru",
+      id: "16",
+    },
+    {
+      nama: "Dr.Mawar Melati Sp.P",
+      tanggal: [
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+        29, 30, 31,
+      ],
+      img: "./assets/img/Dokter/21.jpg",
+      spesialis: "Dokter Paru",
+      id: "17",
+    },
+    {
+      nama: "Dr.Bagus Salim Sp.P",
+      tanggal: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        31,
+      ],
+      img: "./assets/img/Dokter/22.jpg",
+      spesialis: "Dokter Paru",
+      id: "18",
+    },
+  ],
+};
+
+// DOM EKSTERNAL utk menghubungkan dan mengelola file html di JS
+const tanggal = document.querySelector("form");
+
+tanggal.addEventListener("submit", (e) => {
+  e.preventDefault();
+  document.querySelector(".row").innerHTML = "";
+  const data = new FormData(tanggal);
+  const objData = Object.fromEntries(data);
+  localStorage.setItem("jadwal", objData.birthday);
+  let tanggalHari = objData.birthday[8] + objData.birthday[9];
+  if (objData.birthday[8] === "0") {
+    tanggalHari = objData.birthday[9];
+  }
+  localStorage.setItem("tanggal", tanggalHari);
+  const spesialisasi = localStorage.getItem("spesialis");
+  console.log(spesialisasi);
+
+  for (let dokters of dokter[spesialisasi]) {
+    for (let tanggalTugas of dokters.tanggal) {
+      console.log(tanggalHari, "===", String(tanggalTugas));
+      if (tanggalHari === String(tanggalTugas)) {
+        let insert = `
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                <div class="member">
+                    <a href="Form.html" onclick="localStorage.setItem('id', ${dokters.id})"><img src="${dokters.img}" alt="">
+                    <h4>${dokters.nama}</h4></a>
+                    <span >${dokters.spesialis}</span>
+                </div>
+                </div>
+                `;
+        document.querySelector(".row").innerHTML += insert;
+      }
+    }
+  }
+});
+
+const pilihDokter = document.querySelector(".pilihDokter");
+
+pilihDokter.addEventListener("click", (e) => {
+  e.preventDefault();
+  const id = pilihDokter.getAttribute("id");
+  alert(pilihDokter);
+});
---
+++ Hackathons/Hackathon/forms/script.js
@@ -0,0 +1,86 @@
+let dokter = [{
+        nama: "A",
+        Spesialisasi: "Dokter Jantung",
+        Tanggal: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }, {
+        nama: "B",
+        Spesialisasi: "Dokter Jantung",
+        Tanggal: [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    }, {
+        nama: "C",
+        Spesialisasi: "Dokter Jantung",
+        Tanggal: [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+    },
+
+
+    {
+        nama: "A",
+        Spesialisasi: "Dokter Anak",
+        Tanggal: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }, {
+        nama: "B",
+        Spesialisasi: "Dokter Anak",
+        Tanggal: [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    }, {
+        nama: "C",
+        Spesialisasi: "Dokter Anak",
+        Tanggal: [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+    },
+
+    {
+        nama: "A",
+        Spesialisasi: "Dokter Kulit",
+        Tanggal: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }, {
+        nama: "B",
+        Spesialisasi: "Dokter Kulit",
+        Tanggal: [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    }, {
+        nama: "C",
+        Spesialisasi: "Dokter Kulit",
+        Tanggal: [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+    },
+
+    {
+        nama: "A",
+        Spesialisasi: "Dokter THT",
+        Tanggal: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }, {
+        nama: "B",
+        Spesialisasi: "Dokter THT",
+        Tanggal: [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    }, {
+        nama: "C",
+        Spesialisasi: "Dokter THT",
+        Tanggal: [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+    },
+
+    {
+        nama: "A",
+        Spesialisasi: "Dokter Mata",
+        Tanggal: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }, {
+        nama: "B",
+        Spesialisasi: "Dokter Mata",
+        Tanggal: [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    }, {
+        nama: "C",
+        Spesialisasi: "Dokter Mata",
+        Tanggal: [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+    },
+
+    {
+        nama: "A",
+        Spesialisasi: "Dokter Paru",
+        Tanggal: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }, {
+        nama: "B",
+        Spesialisasi: "Dokter Paru",
+        Tanggal: [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    }, {
+        nama: "C",
+        Spesialisasi: "Dokter Paru",
+        Tanggal: [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+    }
+
+]
